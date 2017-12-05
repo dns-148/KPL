5bc301d2d9e3cfe3638815b60abe5aceea1c0da5
mathovonic96 <mathovonic96@gmail.com>
2017-12-06 06:48:53 +0700

    [Tambah] menambah DrawRectangleCommand

---
+++ EasyPaint/EasyPaint/Commands/DrawRectangleCommand.cs
@@ -0,0 +1,40 @@
+﻿using EasyPaint.InterfaceClass;
+using EasyPaint.Shapes;
+
+namespace EasyPaint.Commands
+{
+    public class DrawRectangleCommand
+    {
+        private Canvas ActiveCanvas;
+        private Rectangle RectangleShape;
+        private int X;
+        private int Y;
+        private int Width;
+        private int Height;
+
+
+        public DrawRectangleCommand(Canvas InputCanvas, int XPoint, int YPoint, int Width, int Height)
+        {
+            this.ActiveCanvas = InputCanvas;
+            this.Width = Width;
+            this.Height = Height;
+            this.X = XPoint;
+            this.Y = YPoint;
+        }
+
+        public void Execute()
+        {
+            RectangleShape = new Rectangle(X, Y, Width, Height);
+            this.ActiveCanvas.AddDrawnShape(this.RectangleShape);
+            RectangleShape.Select();
+        }
+
+        public void UnExecute()
+        {
+            if (RectangleShape != null)
+            {
+                this.ActiveCanvas.RemoveDrawnShape(this.RectangleShape);
+            }
+        }
+    }
+}
