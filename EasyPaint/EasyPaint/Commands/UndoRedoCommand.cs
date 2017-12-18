2368e182531d265cf1f630f0f1fbb4916732f634
ujik1996 <cc.ujik@gmail.com>
2017-12-19 00:28:00 +0700

    Logic undo redo

---
+++ EasyPaint/EasyPaint/Commands/UndoRedoCommand.cs
@@ -0,0 +1,28 @@
+﻿using EasyPaint.InterfaceClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyPaint.Commands
+{
+    class UndoRedoCommand : ICommand
+    {
+
+        private Canvas ActiveCanvas;
+        UndoRedoCommand(Canvas canvas)
+        {
+            this.ActiveCanvas = canvas;
+        }
+        public void Execute()
+        {
+            ActiveCanvas.Undo();
+        }
+
+        public void UnExecute()
+        {
+            ActiveCanvas.Redo();
+        }
+    }
+}
